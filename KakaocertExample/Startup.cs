316dc5304f6e7deb9ec50641b7d274b52b17aacd
seunghwan <lshk955@naver.com>
2023-03-21 13:56:22 +0900

    example 최초등록

---
+++ KakaocertExample/Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Kakaocert;
+
+public class KakaocertInstance
+{
+    // 파트너 신청 후 메일로 발급받은 링크아이디(LinkID)와 비밀키(SecretKey)값 으로 변경하시기 바랍니다.
+    private string linkID = "BKAKAO";
+    private string secretKey = "egkxYN99ZObjLa3c0nr9/riG+a0VDkZu87LSGR8c37U=";
+
+    public KakaocertService kakaocertService;
+
+    public KakaocertInstance()
+    {
+        //Kakaocert 서비스 객체 초기화
+        kakaocertService = new KakaocertService(linkID, secretKey);
+
+        //인증토큰의 IP제한기능 사용여부, 권장(true)
+        kakaocertService.IPRestrictOnOff = true;
+
+        //카카오써트 API 서비스 고정 IP 사용여부, true-사용, false-미사용, 기본값(false)
+        kakaocertService.UseStaticIP = false;
+
+        // 로컬시스템 시간 사용 여부 true-사용, false-미사용, 기본값(true)
+        kakaocertService.UseLocalTimeYN = true;
+    }
+}
+
+namespace KakaocertExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSingleton<KakaocertInstance>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Kakaocert}/{action=Index}");
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# barocert.example.dotnetcore
+
+
+### Barocert API Example for ASP.NET Core
+
+
+#### install .NET CLI
+    $ dotnet add package Barocert --version 1.0.0
+
+사이트
+-------------------------------
+* 링크허브 <https://www.linkhub.co.kr/>
+* Barocert <https://developers.barocert.com>
+* Barocert Partner 관리자 <	https://partner.barocert.com>
